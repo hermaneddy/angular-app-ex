263a8ab58f0b3b01104383c15204789fa36d84d0
eduard herman <herman_eduard2000@yahoo.com>
2015-07-03 12:07:08 +0200

    v2

--- SimpleController.js
+++ SimpleController.js
@@ -1,6 +1,26 @@
-var demoApp = angular.module('demoApp', []);
+var demoApp = angular.module('demoApp', ['ngRoute']);
 
+demoApp.config(function($routeProvider){
+	$routeProvider
+		.when('/view1', 
 
+				{
+					controller: 'SimpleController',
+					templateUrl: 'partials/view1.html'
+				}
+
+			)
+        .when('/view2', 
+
+				{
+					controller: 'SimpleController',
+					templateUrl: 'partials/view2.html'
+				}
+
+			)
+        .otherwise({redirectTo: '/'});
+
+});
 
 demoApp.controller()        
 
@@ -12,6 +32,20 @@ $scope.customers = [
                 { name: 'Dave Jones', city: 'Kentucky' },
                 { name: 'Mister X', city: 'SF' }
             ];
+
+$scope.addCustomer = function(){
+
+	$scope.customers.push({
+		name: $scope.newCustomer.name,
+		city: $scope.newCustomer.city
+	    }
+		);
+
+};
+
+
+
 }
+
 
 demoApp.controller(controllers);
